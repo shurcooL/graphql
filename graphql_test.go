f12b317a6da7dfe1b56a4f939958463c7c111e17
Leslie Qi Wang <qiwa@pensando.io>
2021-11-01 12:36:24 -0700

    replace with fork repo package
    Signed-off-by: Leslie Qi Wang <qiwa@pensando.io>

--- graphql_test.go
+++ graphql_test.go
@@ -8,7 +8,7 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	"github.com/shurcooL/graphql"
+	"github.com/leslie-qiwa/graphql"
 )
 
 func TestClient_Query_partialDataWithErrorResponse(t *testing.T) {
--- ident/ident_test.go
+++ ident/ident_test.go
@@ -5,7 +5,7 @@ import (
 	"reflect"
 	"testing"
 
-	"github.com/shurcooL/graphql/ident"
+	"github.com/leslie-qiwa/graphql/ident"
 )
 
 func Example_lowerCamelCaseToMixedCaps() {
